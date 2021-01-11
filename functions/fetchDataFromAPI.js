7477c0be42f8ecbca1c9d3eb791645dbbaffeb58
Ikraam <IbnDaanis@Gmail.com>
2021-01-11 23:04:33 +0000

    fetchData function updated

--- functions/fetchDataFromAPI.js
+++ functions/fetchDataFromAPI.js
@@ -1,24 +1,16 @@
 exports.handler = async function (event, context) {
-  const body = JSON.parse(event.body)
   const prayerTimes = []
   const API_KEY = process.env.API_KEY
-  try {
-    const fetchData = await fetch(
-      `https://www.londonprayertimes.com/api/times/?format=json&key=${API_KEY}&24hours=true`
-    )
-    const dataJSON = await fetchData.json()
-    const salahNames = ['fajr', 'sunrise', 'dhuhr', 'asr', 'magrib', 'isha']
-    salahNames.forEach(salahName => {
-      prayerTimes.push(dataJSON[salahName])
-    })
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ message: prayerTimes, status: 'success' }),
-    }
-  } catch (error) {
-    return {
-      statusCode: 404,
-      body: JSON.stringify({ message: error, status: 'failure' }),
-    }
+  const fetchData = await fetch(
+    `https://www.londonprayertimes.com/api/times/?format=json&key=${API_KEY}&24hours=true`
+  )
+  const dataJSON = await fetchData.json()
+  const salahNames = ['fajr', 'sunrise', 'dhuhr', 'asr', 'magrib', 'isha']
+  salahNames.forEach(salahName => {
+    prayerTimes.push(dataJSON[salahName])
+  })
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ message: prayerTimes, status: 'success' }),
   }
 }
