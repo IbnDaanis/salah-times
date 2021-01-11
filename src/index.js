b5a520b3b7adcfc6b9a557295dd85227a018faff
Ikraam <IbnDaanis@Gmail.com>
2021-01-11 22:33:32 +0000

    Function test 2

--- src/index.js
+++ src/index.js
@@ -1,20 +1,21 @@
-const fetchDataFromAPI = async () => {
-  const prayerTimes = []
-  const API_KEY = process.env.API_KEY
-  try {
-    const fetchData = await fetch(
-      `https://www.londonprayertimes.com/api/times/?format=json&key=${API_KEY}&24hours=true`
-    )
-    const dataJSON = await fetchData.json()
-    const salahNames = ['fajr', 'sunrise', 'dhuhr', 'asr', 'magrib', 'isha']
-    salahNames.forEach(salahName => {
-      prayerTimes.push(dataJSON[salahName])
-    })
-    return prayerTimes
-  } catch (error) {
-    console.error(error)
-  }
-}
+// const fetchDataFromAPI = async () => {
+//   const prayerTimes = []
+//   const API_KEY = process.env.API_KEY
+//   try {
+//     const fetchData = await fetch(
+//       `https://www.londonprayertimes.com/api/times/?format=json&key=${API_KEY}&24hours=true`
+//     )
+//     const dataJSON = await fetchData.json()
+//     const salahNames = ['fajr', 'sunrise', 'dhuhr', 'asr', 'magrib', 'isha']
+//     salahNames.forEach(salahName => {
+//       prayerTimes.push(dataJSON[salahName])
+//     })
+//     return prayerTimes
+//   } catch (error) {
+//     console.error(error)
+//   }
+// }
+import { fetchDataFromAPI2 } from '../functions/fetchDataFromAPI'
 
 const displayDataInDOM = async data => {
   const salahTimesList = document.querySelector('.salah-times-list')
@@ -29,8 +30,4 @@ const displayDataInDOM = async data => {
   document.querySelector('.container').style.opacity = '1'
 }
 
-fetchDataFromAPI().then(data => displayDataInDOM(data))
-
-import { fetchDataFromAPI2 } from '../functions/fetchDataFromAPI'
-
-console.log(fetchDataFromAPI2())
+fetchDataFromAPI2().then(data => displayDataInDOM(data))
