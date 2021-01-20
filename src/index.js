36246e6ba8b923dbd94c78e6aaec041145d0130d
Ikraam <IbnDaanis@Gmail.com>
2021-01-20 21:19:42 +0000

    Added more mobile responsiveness and fetch for developement

--- src/index.js
+++ src/index.js
@@ -11,6 +11,19 @@ const displayDataInDOM = async data => {
   document.querySelector('.container').style.opacity = '1'
 }
 
-fetch('/.netlify/functions/fetchDataFromAPI')
+// fetch('/.netlify/functions/fetchDataFromAPI')
+//   .then(response => response.json())
+//   .then(data => displayDataInDOM(data.message))
+
+const API_ENDPOINT = `https://www.londonprayertimes.com/api/times/?format=json&key=${process.env.SALAH_API_KEY}&24hours=true`
+
+fetch(API_ENDPOINT)
   .then(response => response.json())
-  .then(data => displayDataInDOM(data.message))
+  .then(data => {
+    const prayerTimes = []
+    const salahNames = ['fajr', 'sunrise', 'dhuhr', 'asr', 'magrib', 'isha']
+    salahNames.forEach(salahName => {
+      prayerTimes.push(data[salahName])
+    })
+    displayDataInDOM(prayerTimes)
+  })
